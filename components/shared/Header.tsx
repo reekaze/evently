2203465031a8216e6a8ba8cd50277e012e5d3563
reekaze <tan.albert53@gmail.com>
2023-12-26 17:02:27 +0700

    second commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
     return (
@@ -12,10 +14,15 @@ const Header = () => {
                     <Image alt='Evently logo' src={"/assets/images/logo.svg"} width={128} height={38}>
                     </Image>
                 </Link>
-
+                <SignedIn>
+                    <nav className='md:flex-between hidden w-full max-w-xs'>
+                        <NavItems />
+                    </nav>
+                </SignedIn>
                 <div className='flex w-32 justify-end gap-3'>
                     <SignedIn>
                         <UserButton afterSignOutUrl='/' />
+                        <MobileNav />
                     </SignedIn>
                     <SignedOut>
                         <Button asChild className='rounded-full' size={'lg'}>
