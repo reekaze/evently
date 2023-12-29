24a00f7ed5c498f87a5095a2394bd7b188b925e1
reekaze <tan.albert53@gmail.com>
2023-12-29 12:43:35 +0700

    sixth commit

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 const CreateEvent = () => {
     const {sessionClaims} = auth()
     const userId = sessionClaims?.userId as string;
+    console.log(userId)
     
     return (
         <>
--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,8 +1,9 @@
-import React, { startTransition, useState } from 'react'
+import React, { startTransition, useEffect, useState } from 'react'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from "@/components/ui/select"
 import { ICategory } from '@/lib/database/models/category.model'
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger, } from "@/components/ui/alert-dialog"
 import { Input } from '../ui/input'
+import { createCategory, getAllCategories } from '@/lib/actions/category.action'
 
 
 type Props = {
@@ -15,8 +16,24 @@ const Dropdown = ({ value, onChangeHandler }: Props) => {
     const [newCategory, setNewCategory] = useState('')
 
     const handleAddCategory = () => {
-
+        createCategory({
+            categoryName: newCategory.trim()
+        }).then((category) => {
+            setCategories((prevState) => [...prevState, category])
+        })
     }
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categoryList = await getAllCategories()
+
+            categoryList && setCategories(categoryList as ICategory[])
+        }
+        getCategories()
+
+        return () => {
+        }
+    }, [])
 
 
     return (
@@ -30,9 +47,9 @@ const Dropdown = ({ value, onChangeHandler }: Props) => {
                         {category.name}
                     </SelectItem>
                 ))}
-
+                
                 <AlertDialog>
-                    <AlertDialogTrigger className='p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500'>Open</AlertDialogTrigger>
+                    <AlertDialogTrigger className='p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500'>Add new category</AlertDialogTrigger>
                     <AlertDialogContent className='bg-white'>
                         <AlertDialogHeader>
                             <AlertDialogTitle>New Category</AlertDialogTitle>
